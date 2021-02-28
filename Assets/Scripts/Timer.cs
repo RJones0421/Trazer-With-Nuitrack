c836ff5eddc0962eed277e4f89407bfec9eee942
Tyler Do <tdo2018@fau.edu>
2021-02-27 21:25:01 -0500

    Time
    Time script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timeText;
+    public float startTime;
+    bool timerActive = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText.text = startTime.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerActive)
+        {
+            startTime += Time.deltaTime;
+            timeText.text = startTime.ToString("F2");
+        }
+    }
+}
