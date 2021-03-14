5371c03a23eb2995b9f0e03d4dfe514721604b1b
Tyler Do <tdo2018@fau.edu>
2021-03-14 12:54:58 -0400

    Ui update and db
    json added to 5-10-5
    main menu nav added to 5-10-5
    ui updates

---
+++ Assets/Scripts/StructureObjectSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StructureObjectSpawner : ObjectSpawner
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateColliders();
+        StartCoroutine(StructuredSpawn());
+    }
+
+    private IEnumerator StructuredSpawn() {
+        int count = 0;
+        for (int i = 1; i <= numberOfTests; i++)
+        {
+            timer.StartTime();
+            while (count < spawners.Length) {
+                target.transform.position = spawners[count].transform.position;
+                target.gameObject.SetActive(true);
+
+                while (!targetHit) {
+                    yield return 0;
+                }
+
+                target.gameObject.SetActive(false);
+                targetHit = false;
+                count++;
+            }
+
+            timer.StopTime();
+            count = 0;
+            yield return new WaitForSeconds(2);
+        }
+        Debug.Log("Done");
+        timer.AverageTime();
+        timer.Save();
+        timer.GameOver();
+    }
+
+      public void Restart(){
+        timer.Clear();
+        Start();
+    }
+}
